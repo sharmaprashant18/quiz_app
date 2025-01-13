06bebd8484ad2b5a400a16244528e34e53e82d3c
sharmaprashant18 <ps3348644@gmail.com>
2025-01-13 19:02:34 +0545

    Changed

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -6,20 +6,17 @@ class Homepage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: Scaffold(
-      backgroundColor: Colors.amber,
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-        title: Text('Play Quiz'),
-      ),
-      body: Container(
-          child: SingleChildScrollView(
-        scrollDirection: Axis.horizontal,
-        physics: ScrollPhysics(parent: BouncingScrollPhysics()),
-        child: Row(
-          children: [],
-        ),
-      )),
-    ));
+      child: Scaffold(
+          backgroundColor: Colors.amber,
+          appBar: AppBar(
+            backgroundColor: Colors.amber,
+            title: Text('Play Quiz'),
+          ),
+          body: Container(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+            ),
+          )),
+    );
   }
 }
