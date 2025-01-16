fdc3b4c74473ee41a098188b8c73c1a892587bbd
sharmaprashant18 <ps3348644@gmail.com>
2025-01-16 19:59:25 +0545

    Changed

--- lib/view/homepage.dart
+++ lib/view/homepage.dart
@@ -203,3 +203,12 @@ class CategoryCard extends StatelessWidget {
     );
   }
 }
+
+class MyWidget extends StatelessWidget {
+  const MyWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
