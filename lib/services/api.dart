6f75c968fcd2e77842795a3621b3760d60f06653
sharmaprashant18 <ps3348644@gmail.com>
2025-01-19 09:08:43 +0545

    Changed

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -1,23 +1,23 @@
-import 'package:dio/dio.dart';
-import 'package:quiz_app/model/questions.dart';
+// import 'package:dio/dio.dart';
+// import 'package:quiz_app/model/questions.dart';
 
-class QuizApi {
-  final Dio _dio = Dio();
+// class QuizApi {
+//   final Dio _dio = Dio();
 
-  Future<List<Question>> fetchQuestions() async {
-    try {
-      final response = await _dio.get('https://opentdb.com/api.php?amount=50');
-      if (response.statusCode == 200) {
-        final data = response.data;
-        final questions = (data['results'] as List)
-            .map((json) => Question.fromJson(json))
-            .toList();
-        return questions;
-      } else {
-        throw Exception('Failed to load questions');
-      }
-    } catch (e) {
-      throw Exception('Error fetching questions: $e');
-    }
-  }
-}
+//   Future<List<Question>> fetchQuestions() async {
+//     try {
+//       final response = await _dio.get('https://opentdb.com/api.php?amount=50');
+//       if (response.statusCode == 200) {
+//         final data = response.data;
+//         final questions = (data['results'] as List)
+//             .map((json) => Question.fromJson(json))
+//             .toList();
+//         return questions;
+//       } else {
+//         throw Exception('Failed to load questions');
+//       }
+//     } catch (e) {
+//       throw Exception('Error fetching questions: $e');
+//     }
+//   }
+// }
