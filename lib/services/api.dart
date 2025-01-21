cc959214bc39c31629e99dae85a71b0a185730c7
sharmaprashant18 <ps3348644@gmail.com>
2025-01-21 20:19:14 +0545

    Changed

--- lib/services/api.dart
+++ lib/services/api.dart
@@ -22,12 +22,3 @@ class QuizApi {
     }
   }
 }
-
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
-  }
-}
